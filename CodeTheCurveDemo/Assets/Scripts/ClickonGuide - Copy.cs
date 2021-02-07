dbd24740ddcffdbc636595a61271237b446029c4
iExploze <54289660+iExploze@users.noreply.github.com>
2021-02-07 12:51:38 -0500

    Tour Guide code from Discord Files
    how to use is in the comments

---
+++ CodeTheCurveDemo/Assets/Scripts/ClickonGuide - Copy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickonGuide : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Transform cameraTransform = Camera.main.transform;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag("Guide")) 
+                {
+                    Debug.Log("Clicked");
+                    hit.collider.GetComponent<GuideNav>().GetClicked(true);
+                }
+            }
+        }
+    }
+}
