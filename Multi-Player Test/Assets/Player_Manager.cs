da653c0e5d58554395b4dcbf99314abc6016c408
szhang1021 <52924435+szhang1021@users.noreply.github.com>
2021-02-22 01:10:55 +0800

    Added completed multi-player test

---
+++ Multi-Player Test/Assets/Player_Manager.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.MyGame
+{
+    public class Player_Manager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+        // Start is called before the first frame update
+
+        #region IPunObservable implementation
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(IsFiring);
+                stream.SendNext(Health);
+            }
+            else
+            {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+            }
+        }
+        #endregion
+
+
+
+        [Tooltip("The current Health of our player")]
+        public float Health = 1f;
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        public GameObject PlayerUiPrefab;
+
+
+#if UNITY_5_4_OR_NEWER
+        void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+                {
+                    this.CalledOnLevelWasLoaded(scene.buildIndex);
+                }
+        #endif
+
+
+
+        #region Private Fields
+
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        private GameObject playerUiPrefab;
+
+        [Tooltip("The Beams GameObject to control")]
+        [SerializeField]
+        private GameObject beams;
+
+
+
+        //True, when the user is firing
+        bool IsFiring;
+
+        #endregion
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                this.ProcessInputs();
+                if (Health <= 0f)
+                {
+                    Game_Manager.Instance.LeaveRoom();
+                }
+            }
+
+            if (photonView.IsMine)
+            {
+                this.ProcessInputs();
+                if (Health <= 0f)
+                {
+                    Game_Manager.Instance.LeaveRoom();
+                }
+            }
+
+            if (photonView.IsMine)
+            {
+                ProcessInputs();
+            }
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            Camera_Works _cameraWork = this.gameObject.GetComponent<Camera_Works>();
+
+
+            if (_cameraWork != null)
+            {
+                if (photonView.IsMine)
+                {
+                    _cameraWork.OnStartFollowing();
+                }
+            }
+            else
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            }
+
+            #if UNITY_5_4_OR_NEWER
+                        // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+                        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+            #endif
+
+            void OnLevelWasLoaded(int level)
+            {
+                this.CalledOnLevelWasLoaded(level);
+                GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            }
+
+            if (PlayerUiPrefab != null)
+            {
+                GameObject _uiGo = Instantiate(PlayerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            }
+            else
+            {
+                Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+            }
+
+        }
+
+#if UNITY_5_4_OR_NEWER
+        public override void OnDisable()
+        {
+            // Always call the base to remove callbacks
+            base.OnDisable();
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+        #endif
+
+
+        public void Awake()
+        {
+            if (this.beams == null)
+            {
+                Debug.LogError("<Color=Red><b>Missing</b></Color> Beams Reference.", this);
+            }
+            else
+            {
+                this.beams.SetActive(false);
+            }
+
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                LocalPlayerInstance = gameObject;
+            }
+
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(gameObject);
+
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                Player_Manager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+
+        //if !UNITY_5_4_OR_NEWER
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level);
+        }
+
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+        }
+
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // We are only interested in Beamers
+            // we should be using tags but for the sake of distribution, let's simply check by name.
+            if (!other.name.Contains("Beam"))
+            {
+                return;
+            }
+            Health -= 0.1f;
+        }
+        
+        void OnTriggerStay(Collider other)
+        {
+            // we dont' do anything if we are not the local player.
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // We are only interested in Beamers
+            // we should be using tags but for the sake of distribution, let's simply check by name.
+            if (!other.name.Contains("Beam"))
+            {
+                return;
+            }
+            // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
+            Health -= 0.1f * Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Processes the inputs. This MUST ONLY BE USED when the player has authority over this Networked GameObject (photonView.isMine == true)
+        /// </summary>
+
+        void ProcessInputs()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                // we don't want to fire when we interact with UI buttons for example. IsPointerOverGameObject really means IsPointerOver*UI*GameObject
+                // notice we don't use on on GetbuttonUp() few lines down, because one can mouse down, move over a UI element and release, which would lead to not lower the isFiring Flag.
+                if (EventSystem.current.IsPointerOverGameObject())
+                {
+                    //	return;
+                }
+
+                if (!this.IsFiring)
+                {
+                    this.IsFiring = true;
+                }
+            }
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (this.IsFiring)
+                {
+                    this.IsFiring = false;
+                }
+            }
+        }
+    }
+}
+
