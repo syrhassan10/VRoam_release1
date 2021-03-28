8cef6608b9a460c45c3a91b2090e3ba50297c4c7
urboiJeff2006 <52462206+urboiJeff2006@users.noreply.github.com>
2021-03-28 11:29:31 -0400

    Fixed gay shit

---
+++ VroamServerandClient/VroamServer/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int id;
+    public string username;
+    public CharacterController controller;
+    public Transform shootOrigin;
+
+    public float gravity = -9.81f;
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+    public float health;
+    public float maxHealth = 100f;
+
+    
+    public  string GunEquipped;
+    public Vector3 playerPosition;
+    public Quaternion playerRotation;
+
+
+    public void Initialize(int _id, string _username)
+    {
+        id = _id;
+        username = _username;
+        health = maxHealth;
+
+
+    }
+
+    /// <summary>Processes player input and moves the player.</summary>
+    public void FixedUpdate()
+    {
+
+        transform.position = playerPosition;
+        transform.rotation = playerRotation;
+        UpdatePlayer();
+    }
+
+    private void UpdatePlayer()
+    {
+
+       
+        ServerSend.PlayerPosition(id,this);
+        ServerSend.PlayerRotation(id,this);
+    }
+
+    /// <summary>Updates the player input with newly received input.</summary>
+    /// <param name="_inputs">The new key inputs.</param>
+    /// <param name="_rotation">The new rotation.</param>
+
+
+
+
+
+
+}
---
+++ VroamServerandClient/VroamServer/Assets/Scripts/ServerHandle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerHandle
+{
+    public static void WelcomeReceived(int _fromClient, Packet _packet)
+    {
+        int _clientIdCheck = _packet.ReadInt();
+        string _username = _packet.ReadString();
+
+        Debug.Log($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+        if (_fromClient != _clientIdCheck)
+        {
+            Debug.Log($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+        }
+        Server.clients[_fromClient].SendIntoGame(_username);
+    }
+
+    
+
+
+
+    public static void PlayerPosition(int _fromClient, Packet _packet)
+    {
+        Vector3 _position = _packet.ReadVector3();
+
+        Server.clients[_fromClient].player.playerPosition = _position;
+    }
+    public static void PlayerRotation(int _fromClient, Packet _packet)
+    {
+        Quaternion _rotation = _packet.ReadQuaternion();
+
+        Server.clients[_fromClient].player.playerRotation = _rotation;
+    }
+
+
+
+
+}
